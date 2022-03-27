440b85e34ce671168aae9a74a70b817ed49f6623
Agnieszka <agnieszka.blicharska@gmail.com>
2022-03-27 17:11:13 +0200

    2 modele

--- supportUkraine/podziel_się/models.py
+++ supportUkraine/podziel_się/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class CurrentNeeds(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    amount = models.IntegerField()
+    date = models.DateField(null=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, null=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
--- supportUkraine/podziel_się/views.py
+++ supportUkraine/podziel_się/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django import views
+from django.views.generic.list import ListView
+from podziel_się.models import CurrentNeeds
 
-class MainView(views.View):
-    def get(self, request):
-        return render(request, 'podziel_się/mainview.html')
+
+class MainView(ListView):
+    model = CurrentNeeds
+    template_name = 'podziel_się/mainview.html'
+    context_object_name = 'current_needs'
 
